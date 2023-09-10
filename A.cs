6274b35b6904c764b16cc5f3f8683626766e32d1
dinhcong123 <trandinhcong895@gmail.com>
2023-09-10 21:46:07 +0700

    them class A.cs

---
+++ A.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2180607804_ngothainguyen_Lab01_01
+{
+    internal class A
+    {
+    }
+}
